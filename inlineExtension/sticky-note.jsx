df9364ccc0939d8bd9450afe64619735357ba46c
Ari <arlenehdez0225@gmail.com>
2026-03-04 14:38:44 -0500

    Create sticky-note.jsx

---
+++ inlineExtension/sticky-note.jsx
@@ -0,0 +1,8 @@
+export default function StickyNote() {
+  return (
+    <div className="sticky-note">
+      <h2>Sticky Note</h2>
+      <p>This is a simple sticky note component.</p>
+    </div>
+  )
+}
