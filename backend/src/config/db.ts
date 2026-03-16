342d7d50bf9daa69a0261100d7a08c32bd1b6e44
David Bucio-Paz <daviddbp.buciopaz@gmail.com>
2026-03-16 15:40:36 -0400

    Added supabase, connected supabase.
    Added supabase - connected it with typescript.
    
    going to try and share with other team members

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,17 +1,16 @@
-import dotenv from "dotenv"
-import { createClient } from '@supabase/supabase-js'
-import type {Database} from './database.types'
+import dotenv from "dotenv";
+import { createClient } from "@supabase/supabase-js";
+import type { Database } from "./database.types";
 
-dotenv.config()
+dotenv.config();
 
-const supabaseURL = process.env.SECRET_DATABASE_CONNECTION
-const supabaseKEY = process.env.SECRET_DATABASE_KEY
+const supabaseURL = process.env.SECRET_DATABASE_CONNECTION;
+const supabaseKEY = process.env.SECRET_DATABASE_KEY;
 
 if (!supabaseURL || !supabaseKEY) {
-    throw new Error("Missing Supabase key or url in .env")
+  throw new Error("Missing Supabase key or url in .env");
 }
 
-const supabase = createClient<Database>(supabaseURL, supabaseKEY)
+const supabase = createClient<Database>(supabaseURL, supabaseKEY);
 
-
-export default supabase
+export default supabase;
